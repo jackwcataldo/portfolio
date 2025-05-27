50729d06ed74d2f74cfec94147bd64c037d47bea
Jack Cataldo <jackcataldo@Jacks-MacBook-Pro.local>
2025-05-27 11:08:13 -0400

    Updates

--- precache-manifest.25544a6d2bd801e7fd11807da70542fa.js
+++ precache-manifest.25544a6d2bd801e7fd11807da70542fa.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c2456a07ac7070d32060ef9c245fc3e",
+    "revision": "3d430cc3013e594192f3b908c51c3437",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "99598413e991afda1e6e",
+    "revision": "9773a57c3a0c6d607069",
     "url": "/portfolio/static/css/2.58736d6f.chunk.css"
   },
   {
-    "revision": "15de8efc3fc5a77c30b7",
+    "revision": "faa1f93f9f7b58a2af7a",
     "url": "/portfolio/static/css/main.cc487efa.chunk.css"
   },
   {
-    "revision": "99598413e991afda1e6e",
-    "url": "/portfolio/static/js/2.fb7e8da3.chunk.js"
+    "revision": "9773a57c3a0c6d607069",
+    "url": "/portfolio/static/js/2.0497999f.chunk.js"
   },
   {
     "revision": "46a379a572333860f78765c743603e43",
-    "url": "/portfolio/static/js/2.fb7e8da3.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.0497999f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "15de8efc3fc5a77c30b7",
-    "url": "/portfolio/static/js/main.fb56b14d.chunk.js"
+    "revision": "faa1f93f9f7b58a2af7a",
+    "url": "/portfolio/static/js/main.54ba7538.chunk.js"
   },
   {
     "revision": "58d18144e62526203a32",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6e0c8be9c9181f61c00e8510a1da9c1f.js"
+  "/portfolio/precache-manifest.25544a6d2bd801e7fd11807da70542fa.js"
 );
 
 self.addEventListener('message', (event) => {
