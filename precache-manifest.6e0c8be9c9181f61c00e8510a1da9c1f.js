442466d22d8ff48a066efb57addbaa7937278992
Jack Cataldo <jackcataldo@Jacks-MacBook-Pro.local>
2025-05-27 11:04:11 -0400

    Updates

---
+++ precache-manifest.6e0c8be9c9181f61c00e8510a1da9c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2456a07ac7070d32060ef9c245fc3e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "99598413e991afda1e6e",
+    "url": "/portfolio/static/css/2.58736d6f.chunk.css"
+  },
+  {
+    "revision": "15de8efc3fc5a77c30b7",
+    "url": "/portfolio/static/css/main.cc487efa.chunk.css"
+  },
+  {
+    "revision": "99598413e991afda1e6e",
+    "url": "/portfolio/static/js/2.fb7e8da3.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portfolio/static/js/2.fb7e8da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15de8efc3fc5a77c30b7",
+    "url": "/portfolio/static/js/main.fb56b14d.chunk.js"
+  },
+  {
+    "revision": "58d18144e62526203a32",
+    "url": "/portfolio/static/js/runtime-main.770b1f5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fd85d5967d996b087b1404c4c979d2fc.js"
+  "/portfolio/precache-manifest.6e0c8be9c9181f61c00e8510a1da9c1f.js"
 );
 
 self.addEventListener('message', (event) => {
